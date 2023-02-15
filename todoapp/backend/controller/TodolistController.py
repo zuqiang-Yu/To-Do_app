6f86b41fc581a93a0ac2b796641cebfd7a211ba1
zuqiangyu <zyuu0020@student.monash.edu>
2023-02-15 21:15:28 +1100

    add: TodolistServer.py, encode and decode for user jwt token. feat: when user wanna get their items need to verify jwt token first add: new API for login

--- todoapp/backend/controller/TodolistController.py
+++ todoapp/backend/controller/TodolistController.py
@@ -4,21 +4,23 @@ from todoapp.backend.util.DbConnector import DbConnector
 import psycopg2
 import datetime
 from loguru import logger
+from todoapp.backend.server.TodolistServer import encode_auth_token,decode_auth_token
+from flask import request
 
 app_info = Blueprint("app_info", __name__)
 
 database = DbConnector()
 
 
-@app_info.route("/users", methods=["GET"])
-def get_users():
-    cur = database.getCursor()
-    cur.execute('SELECT * FROM users;')
-    users = cur.fetchall()
-
-    database.close()
-
-    return Response.success(users)
+# @app_info.route("/users", methods=["GET"])
+# def get_users():
+#     cur = database.getCursor()
+#     cur.execute('SELECT * FROM users;')
+#     users = cur.fetchall()
+#
+#     database.close()
+#
+#     return Response.success(users)
 
 
 @app_info.route("/users", methods=["POST"])
@@ -52,36 +54,81 @@ def register_user():
       return Response.success(data)
 
 
+@app_info.route("/login", methods=["GET"])
+def login():
+    # get user input from front-end
+    username = RequestUtils.get("username").lower()
+    password = RequestUtils.get("password").lower()
+
+    # verify username and password
+    cur = database.getCursor()
+    cur.execute('SELECT * FROM users WHERE username=\'' + username + '\';')
+    user = cur.fetchall()
+
+    user_data = {
+      "user_id": user[0][0],
+      "username": user[0][1],
+      "role": user[0][3]
+    }
+
+    # return JWT token
+    if user[0][2] == password:
+      JWTToken = encode_auth_token(user_data)
+      database.close()
+      return Response.success(JWTToken)
+    else:
+      database.close()
+      return Response.fail()
+
+
+
 @app_info.route("/items", methods=["POST"])
 def add_new_item():
-    user_id = RequestUtils.get("user_id").lower()
-    item = RequestUtils.get("item").lower()
-    try:
-        cur = database.getCursor()
-        insert_query = """INSERT INTO items_table(user_id, items, create_date) VALUES (%s,%s,%s)"""
-        insert_data = (user_id, item, datetime.datetime.now())
+    if decode_auth_token(str(request.headers['Jwttoken']))[0] == 200:
+      user_id = RequestUtils.get("user_id").lower()
+      item = RequestUtils.get("item").lower()
+      try:
+          cur = database.getCursor()
+          insert_query = """INSERT INTO items_table(user_id, items, create_date) VALUES (%s,%s,%s)"""
+          insert_data = (user_id, item, datetime.datetime.now())
 
-        cur.execute(insert_query, insert_data)
-        database.connection.commit()
-        count = cur.rowcount
-        print(count, "Record inserted successfully into mobile table")
-    except (Exception, psycopg2.Error) as error:
-        logger.error(error)
-        return Response.fail()
-    finally:
-        database.close()
-        print("PostgreSQL connection is closed")
+          cur.execute(insert_query, insert_data)
+          database.connection.commit()
+          count = cur.rowcount
+          print(count, "Record inserted successfully into mobile table")
+      except (Exception, psycopg2.Error) as error:
+          logger.error(error)
+          return Response.fail()
+      finally:
+          database.close()
+          print("PostgreSQL connection is closed")
 
-    return Response.success(count)
+      return Response.success(count)
+    else:
+      print(decode_auth_token(str(request.headers['Jwttoken']))[1])
+      return Response.fail()
 
 
 @app_info.route("/items", methods=["GET"])
 def get_item_by_user_id():
-    cur = database.getCursor()
-    user_id = RequestUtils.get("user_id").lower()
-    cur.execute('SELECT * FROM items_table WHERE user_id=\'' + user_id + '\';')
-    item = cur.fetchall()
+    #verify JWT token
+    if decode_auth_token(str(request.headers['Jwttoken']))[0] == 200:
 
-    database.close()
+      cur = database.getCursor()
+      user_id = RequestUtils.get("user_id").lower()
+      cur.execute('SELECT * FROM items_table WHERE user_id=\'' + user_id + '\';')
+      item = cur.fetchall()
+      database.close()
+      return Response.success(item)
 
-    return Response.success(item)
+    else:
+      print(decode_auth_token(str(request.headers['Jwttoken']))[1])
+      return Response.fail()
+
+
+
+
+
+
+
+
---
+++ todoapp/backend/server/TodolistServer.py
@@ -0,0 +1,45 @@
+import jwt
+import datetime
+
+
+def encode_auth_token(user_info):
+  """
+  Generates the Auth Token
+  :return: string
+  """
+  try:
+    payload = {
+      'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+      'iat': datetime.datetime.utcnow(),
+      'sub': user_info
+    }
+    return jwt.encode(
+      payload,
+      key='\xcfj\xc8\x04[x\xbc\xb8\x9d7\xa7\xe7\xff\xf9r\x15w\xf4j\xda-\x13F\xaa',
+      algorithm='HS256'
+    )
+  except Exception as e:
+    return e
+
+
+
+def decode_auth_token(auth_token):
+  """
+  Decodes the auth token
+  :param auth_token:
+  :return: integer|string
+  """
+  try:
+    payload = jwt.decode(auth_token, '\xcfj\xc8\x04[x\xbc\xb8\x9d7\xa7\xe7\xff\xf9r\x15w\xf4j\xda-\x13F\xaa', algorithms='HS256')
+    return (200,payload['sub'])
+  except jwt.ExpiredSignatureError:
+    return (400,'Signature expired. Please log in again.')
+  except jwt.InvalidTokenError:
+    return (401,'Invalid token. Please log in again.')
+
+
+# token = encode_auth_token('zuqiang')
+# print(token)
+
+a = decode_auth_token('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NzY0NTM3MTAsImlhdCI6MTY3NjQ1MzcwNSwic3ViIjoienVxaWFuZyJ9.Vkptg-8WyhugUWhzLKphPF7GavaSmbCtOvYMhQpCaZg')
+print(a)
