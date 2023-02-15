5704e28a78e15e8aee8d581c1eb92c988db61245
zuqiangyu <zyuu0020@student.monash.edu>
2023-02-15 19:20:24 +1100

    add: add new python file config.py

--- todoapp/backend/app.py
+++ todoapp/backend/app.py
@@ -2,10 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from todoapp.backend.controller.TodolistController import app_info
+from flask_bcrypt import Bcrypt
+
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 CORS(app, supports_credentials=True)
 # register the blueprint into flask app
 app.register_blueprint(app_info, url_prefix="/data")
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yzq:zuqiangyyy@localhost:5432/flask_db'
 
