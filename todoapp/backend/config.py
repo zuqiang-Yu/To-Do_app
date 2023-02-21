19120866561ce1f7cc00077721a89d54016bd499
zuqiangyu <zyuu0020@student.monash.edu>
2023-02-22 04:27:39 +1100

    add: requirements.txt in my backend

--- todoapp/backend/config.py
+++ todoapp/backend/config.py
@@ -1,3 +1,3 @@
-BCRYPT_LOG_ROUNDS = 0
-SECRET_KEY = ''
-DATABASE_URL = ''
+BCRYPT_LOG_ROUNDS = 10
+SECRET_KEY = '\xcfj\xc8\x04[x\xbc\xb8\x9d7\xa7\xe7\xff\xf9r\x15w\xf4j\xda-\x13F\xaa'
+DATABASE_URL = 'postgresql://yzq:zuqiangyyy@localhost:5432/flask_db'
